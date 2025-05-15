99fb0a447b2afa2272c4c443e35f383cb94a07c1
Raina Moon <mds6425@gmail.com>
2025-05-16 03:58:15 +0900

    🚑 fix: update CORS middleware configuration for improved security

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,12 @@ import authRoutes from "./routes/auth";
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use("/api/user", userRoutes);
 app.use("/api/cola", colaRoutes);
