5a90a1c57469fa600aa4e7ddb294a53fa6cb1354
Raina Moon <mds6425@gmail.com>
2025-05-16 22:05:40 +0900

    ✨ feat: import and invoke notifyActiveUser function in index file

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import userRoutes from "./routes/user";
 import colaRoutes from "./routes/cola";
 import authRoutes from "./routes/auth";
 import notificationRoutes from "./routes/notification";
+import { notifyActiveUser } from "./cron/notifyActiveUser";
 
 dotenv.config();
 const app = express();
@@ -14,7 +15,9 @@ app.use(express.json());
 app.use("/api/user", userRoutes);
 app.use("/api/cola", colaRoutes);
 app.use("/api/auth", authRoutes);
-app.use("/api/notification",notificationRoutes);
+app.use("/api/notification", notificationRoutes);
+
+notifyActiveUser();
 
 const PORT = Number(process.env.PORT) || 5000;
 app.listen(PORT, "0.0.0.0", () =>
